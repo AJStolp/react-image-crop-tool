78d66f0ca5cc8936e6d4f87ff0e9d7993a70cb8b
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-10-22 14:57:31 -0500

    image crooper from react-image-cropper

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import ImageCropTool from "./image-crop-tool/image-crop-tool";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <h3>here</h3>
+      </header> */}
+      <ImageCropTool />
+    </div>
+  );
+}
+
+export default App;
